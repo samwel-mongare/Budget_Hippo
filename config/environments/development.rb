dbbba9fb1bf8192b4bba4c7d57cd0296644d5f84
mong'are <mongaresams@gmail.com>
2022-04-20 08:52:58 +0300

    Add letter opener to dev environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,6 +17,9 @@ Rails.application.configure do
   # Enable server timing
   config.server_timing = true
 
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
