d8b0bc657b770c73e5d1c074ab5b8a3336503913
mong'are <mongaresams@gmail.com>
2022-04-22 18:51:24 +0300

    Add add new transaction functionality

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,7 +1,35 @@
 class ExpensesController < ApplicationController
-  def index; end
+  load_and_authorize_resource
+
+  def index
+    @categories = expense.all
+  end
 
   def show; end
 
-  def new; end
+  def new
+    @expense = Expense.new
+  end
+
+  def create
+    @expense = current_user.expenses.new(expense_params)
+
+    respond_to do |format|
+      format.html do
+        if @expense.save
+          flash[:notice] = 'Transaction added successfully.'
+          redirect_to categories_path
+        else
+          flash[:alert] = 'Transaction creation failed. Try again'
+          render :new, status: :unprocessable_entity
+        end
+      end
+    end
+  end
+
+  private
+
+  def expense_params
+    params.require(:expense).permit(:name, :amount)
+  end
 end
