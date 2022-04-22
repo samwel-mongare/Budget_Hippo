8af6d0a43460c2f7cb2c2cbe4c6d8c54b5433434
mong'are <mongaresams@gmail.com>
2022-04-22 13:59:39 +0300

    Add code to create a new category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,5 +6,24 @@ class CategoriesController < ApplicationController
   end
 
   def new
+    @category = Category.new
+  end
+
+  def create
+    @category = current_user.categories.new(category_params)
+
+    respond_to do |format|
+      if @category.save
+        format.html { redirect_to inventories_path, notice: 'Category created successfully' }
+      else
+        format.html { render :new, status: :unprocessable_entity, alert: 'Category created successfully' }
+      end
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :icon)
   end
 end
