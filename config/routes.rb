c6e4c7040e2ee5ec602c2dc052ce42aad5602fff
mong'are <mongaresams@gmail.com>
2022-04-23 22:52:51 +0300

    Remove users from resources

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,6 @@ Rails.application.routes.draw do
   end
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :users
   resources :categories_expenses
   resources :expenses 
   resources :categories
