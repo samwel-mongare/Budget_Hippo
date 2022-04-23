3a6bca40931836596c1badb38bcad469b1e9e125
mong'are <mongaresams@gmail.com>
2022-04-23 13:25:12 +0300

    Finalize styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
-class ApplicationController < ActionController::Base
-  class ApplicationController < ActionController::Base
+ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
 
     before_action :update_allowed_parameters, if: :devise_controller?
@@ -11,4 +10,3 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
     end
   end
-end
